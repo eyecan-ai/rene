0a93956728a84cdeb1ecf2feb467c941bc6544ef
Marco Toschi <marco.toschi@fastwebnet.it>
2023-04-17 19:56:13 +0200

    added colored outlines to test videos

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,41 +1,55 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-// var INTERP_BASE = "./static/interpolation/stacked";
-// var NUM_INTERP_FRAMES = 240;
+function update_box_size() {
+  let box = document.querySelector('.slider-item');
+  let width = box.offsetWidth;
+  let height = box.offsetHeight;
+  console.log(width, height)
+  let rectangle = document.querySelector('.legend-boxes');
+  rectangle.style.width = width + 'px';
+  rectangle.style.height = height + 'px';
 
-// var interp_images = [];
-// function preloadInterpolationImages() {
-//   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-//     var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-//     interp_images[i] = new Image();
-//     interp_images[i].src = path;
-//   }
-// }
+  var firstLegendBoxes = document.querySelector('.legend-boxes');
 
-// function setInterpolationImage(i) {
-//   var image = interp_images[i];
-//   image.ondragstart = function () { return false; };
-//   image.oncontextmenu = function () { return false; };
-//   $('#interpolation-image-wrapper').empty().append(image);
-// }
+  // Remove the previous clones
+  document.querySelectorAll('.legend-boxes-clone').forEach(e => e.remove());
+
+  // Create a copy of it
+  function add_clone(index) {
+    var clone = firstLegendBoxes.cloneNode(true);
+    marginLeft = clone.marginLeft;
+
+    clone.classList.add("legend-boxes-clone");
+    margin = clone.style.margin;
+    clone.style.marginLeft = index * width + "px"
+    clone.style.marginTop = - height + "px"
+
+    // Inject it into the DOM
+    firstLegendBoxes.after(clone);
+  }
+
+  document.querySelectorAll('.slider-item').forEach((e, index) => {
+    if (index > 1) {
+      add_clone(index - 1);
+    }
+  })
+}
+
+const mutationCallback = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    update_box_size();
+  }
+}
 
 
 $(document).ready(function () {
-  // Check for click events on the navbar burger icon
-  // $(".navbar-burger").click(function () {
-  //   // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-  //   $(".navbar-burger").toggleClass("is-active");
-  //   $(".navbar-menu").toggleClass("is-active");
-
-  // });
-
   const observer = lozad(); // lazy loads elements with default selector as '.lozad'
   observer.observe();
 
   var options = {
     slidesToScroll: 1,
     slidesToShow: 3,
-    loop: true,
+    loop: false,
     infinite: false,
     autoplay: false,
     autoplaySpeed: 3000,
@@ -61,22 +75,19 @@ $(document).ready(function () {
     });
   }
 
-  /*var player = document.getElementById('interpolation-video');
-  player.addEventListener('loadedmetadata', function() {
-    $('#interpolation-slider').on('input', function(event) {
-      console.log(this.value, player.duration);
-      player.currentTime = player.duration / 100 * this.value;
-    })
-  }, false);*/
-  // preloadInterpolationImages();
 
-  // $('#interpolation-slider').on('input', function (event) {
-  //   setInterpolationImage(this.value);
-  // });
-  // setInterpolationImage(0);
-  // $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
   bulmaSlider.attach();
 
-})
+  // Observe the box size change 
+  const another_observer = new MutationObserver(mutationCallback)
+  toObserve_1 = document.querySelector('.lozad');
+  toObserve_2 = document.querySelector('.slider-item');
+  another_observer.observe(toObserve_1, { attributes: true })
+  another_observer.observe(toObserve_2, { attributes: true })
+  update_box_size();
 
+
+
+
+})
