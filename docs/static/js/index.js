d822851cda71648b2488cfb3adc639bfc75fe565
Marco Toschi <marco.toschi@fastwebnet.it>
2023-04-20 17:25:03 +0200

    fix: :fire: fix height of overlays on page loading

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,36 +1,58 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
+// Debounce bulma carousel next/prev to prevent race condition.
+bulmaCarousel.prototype.next = debounce(bulmaCarousel.prototype.next, 300, true);
+bulmaCarousel.prototype.prev = debounce(bulmaCarousel.prototype.prev, 300, true);
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
 function update_box_size() {
   let box = document.querySelector('.slider-item');
   let width = box.offsetWidth;
-  let height = box.offsetHeight;
-  console.log(width, height)
-  let rectangle = document.querySelector('.legend-boxes');
-  rectangle.style.width = width + 'px';
-  rectangle.style.height = height + 'px';
 
-  var firstLegendBoxes = document.querySelector('.legend-boxes');
+  // let height = box.offsetHeight;
+  // Videos are lazy loading, until we get how to catch when video is fully loaded
+  // we constrain height to be the right ratio cause width is loading correctly
+  // while height seems to be halved
+  ratio = 359 / 320
+  let height = ratio * width;
 
-  // Remove the previous clones
-  document.querySelectorAll('.legend-boxes-clone').forEach(e => e.remove());
+  let boxes = document.querySelector('.legend-boxes');
+  boxes.style.width = width + 'px';
+  boxes.style.height = height + 'px';
 
-  // Create a copy of it
-  function add_clone(index) {
-    var clone = firstLegendBoxes.cloneNode(true);
+  var overlay = document.querySelector('.is-overlay');
+
+  // // Remove the previous clones
+  document.querySelectorAll('.is-overlay-clone').forEach(e => e.remove());
+
+  // // Create a copy of it
+  function add_clone(element, index) {
+    var clone = overlay.cloneNode(true);
     marginLeft = clone.marginLeft;
 
-    clone.classList.add("legend-boxes-clone");
+    clone.classList.add("is-overlay-clone");
     margin = clone.style.margin;
     clone.style.marginLeft = index * width + "px"
-    clone.style.marginTop = - height + "px"
-
-    // Inject it into the DOM
-    firstLegendBoxes.after(clone);
+    element.append(clone)
   }
 
-  document.querySelectorAll('.slider-item').forEach((e, index) => {
-    if (index > 1) {
-      add_clone(index - 1);
+  document.querySelectorAll('.item').forEach((element, index) => {
+    if (index > 0) {
+      add_clone(element, index);
     }
   })
 }
@@ -47,47 +69,21 @@ $(document).ready(function () {
   observer.observe();
 
   var options = {
+    initialSlide: 0,
     slidesToScroll: 1,
     slidesToShow: 3,
-    loop: false,
+    loop: true,
     infinite: false,
     autoplay: false,
     autoplaySpeed: 3000,
+    breakpoints: [{ changePoint: 480, slidesToShow: 1, slidesToScroll: 1 }, { changePoint: 768, slidesToShow: 2, slidesToScroll: 1 }],
   }
 
   // Initialize all div with carousel class
-  var carousels = bulmaCarousel.attach('.carousel', options);
-
-  // Loop on each carousel initialized
-  for (var i = 0; i < carousels.length; i++) {
-    // Add listener to  event
-    carousels[i].on('before:show', state => {
-      console.log(state);
-    });
-  }
-
-  // Access to bulmaCarousel instance of an element
-  var element = document.querySelector('#my-element');
-  if (element && element.bulmaCarousel) {
-    // bulmaCarousel instance is available as element.bulmaCarousel
-    element.bulmaCarousel.on('before-show', function (state) {
-      console.log(state);
-    });
-  }
-
-
-
-  bulmaSlider.attach();
+  bulmaCarousel.attach('#results-carousel', options);
 
   // Observe the box size change 
-  const another_observer = new MutationObserver(mutationCallback)
-  toObserve_1 = document.querySelector('.lozad');
+  const another_observer_2 = new MutationObserver(mutationCallback)
   toObserve_2 = document.querySelector('.slider-item');
-  another_observer.observe(toObserve_1, { attributes: true })
-  another_observer.observe(toObserve_2, { attributes: true })
-  update_box_size();
-
-
-
-
+  another_observer_2.observe(toObserve_2, { attributes: true, subtree: true, })
 })
